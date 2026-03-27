7a67c4d9eefb36072d2ff5f940adee6357497f29
Bennett Parker <143557544+broicool@users.noreply.github.com>
2026-03-27 07:49:59 -0600

    something broke

--- infinte-crawler/Assets/Scripts/Bullet.cs
+++ infinte-crawler/Assets/Scripts/Bullet.cs
@@ -12,10 +12,6 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rigi = GetComponent<Rigidbody2D>();
-<<<<<<< Updated upstream
-=======
-        // set velocity directly for consistent 2D movement
->>>>>>> Stashed changes
         Destroy(gameObject, lifeSpam);
     }
 
