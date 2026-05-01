90c83d11600b5eddfc611b3c244ced083c9078f6
Bennett Parker <143557544+broicool@users.noreply.github.com>
2026-05-01 07:43:50 -0600

    killed sword

--- infinte-crawler/Assets/Scripts/PlayerMoveTopDown.cs
+++ infinte-crawler/Assets/Scripts/PlayerMoveTopDown.cs
@@ -30,10 +30,6 @@ public class PlayerMoveTopDown : MonoBehaviour
     private float coins = 0;
     public TextMeshProUGUI coinsText;
     public TextMeshProUGUI ammoText;
-    public GameObject sword;
-    public float swingSpeed;
-    public bool isSwinging; //Debuging
-    public GameObject realSword;
     
     
     
@@ -174,8 +170,6 @@ public class PlayerMoveTopDown : MonoBehaviour
 
         coinsText.text = "0";
 
-        isSwinging = false;
-
         
     }
 
@@ -245,16 +239,6 @@ public class PlayerMoveTopDown : MonoBehaviour
         }
     }
 
-
-    
-    void swingSword()
-    {
-        isSwinging = true;
-        sword.GetComponent<BoxCollider2D>().enabled = true;
-        sword.transform.Rotate(0, 0, swingSpeed * Time.deltaTime);
-        isSwinging = false;
-    }
-
     void Update()
     {
         /*** Movement for player ***/
@@ -283,34 +267,6 @@ public class PlayerMoveTopDown : MonoBehaviour
         moveVect.y = moveVect.y * speed * Time.deltaTime;
         moveVect.x = moveVect.x * speed * Time.deltaTime;
         rigi.AddForce(moveVect);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            swingSword();
-        }
-       
-        if(!isSwinging){
-            Quaternion swordRotation = Quaternion.identity;
-
-            if(direction == "up")
-            {
-                swordRotation = Quaternion.Euler(0, 0, 90);
-            } 
-            else if(direction == "down")
-            {
-                swordRotation = Quaternion.Euler(0, 0, 270);
-            } 
-            else if(direction == "right")
-            {
-                swordRotation = Quaternion.Euler(0, 0, 0);
-            }
-            else if(direction == "left")
-            {
-                swordRotation = Quaternion.Euler(0, 0, 180);
-            }
-            sword.transform.rotation = swordRotation;
-            sword.GetComponent<BoxCollider2D>().enabled = false;
-        }
     }
     
     
